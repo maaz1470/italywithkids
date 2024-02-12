b0834eb76277ed9a90b9b508730b7d9a15dc0f18
Rahat Hossain <webmaazgmail.com>
2024-02-12 10:34:26 +0600

    fix load error

--- assets/js/Home/home.js
+++ assets/js/Home/home.js
@@ -1,14 +1,9 @@
-window.onload = function(){
-
-
+window.addEventListener("load", function () {
     setTimeout(() => {
-        // Main Css
-        const mainStyle = document.createElement('link');
-        mainStyle.rel = 'stylesheet';
-        mainStyle.href = 'https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,700;1,400&family=Noto+Sans+JP:wght@400;500;700;900&display=swap';
-        document.head.appendChild(mainStyle);
-    }, 3000)
-
-
-
-}
+        // Google Font
+        const google_font = document.createElement('link');
+        google_font.rel = 'stylesheet';
+        google_font.href = 'https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,700;1,400&family=Noto+Sans+JP:wght@400;500;700;900&display=swap';
+        document.head.appendChild(google_font);
+    }, 4000)
+})
