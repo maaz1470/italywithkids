cf27881ce61d0dac5faa42ee85ebabd10d547648
Rahat Hossain <webmaazgmail.com>
2024-02-12 10:18:59 +0600

    added home.js for performance

---
+++ assets/js/Home/home.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+
+
+    setTimeout(() => {
+        // Main Css
+        const mainStyle = document.createElement('link');
+        mainStyle.rel = 'stylesheet';
+        mainStyle.href = 'https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,700;1,400&family=Noto+Sans+JP:wght@400;500;700;900&display=swap';
+        document.head.appendChild(mainStyle);
+    }, 3000)
+
+
+
+}
