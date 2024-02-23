b995c931b463e241a558211ac8ad9a3c17ba2570
Rahat Hossain <webmaazgmail.com>
2024-02-23 23:31:05 +0600

    update

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
 
-define("NITROPACK_HOME_URL", "Your home page URL"); 
-define("NITROPACK_SITE_ID", "your site ID"); 
-define("NITROPACK_SITE_SECRET", "your site secret"); 
+define("NITROPACK_HOME_URL", "maaz1470.github.io/italywithkids"); 
+define("NITROPACK_SITE_ID", "rNGmOnyyRYZudDLBKLLmSrqnVfBByCmV"); 
+define("NITROPACK_SITE_SECRET", "dMZFFuk5F8J1nhKYn6qEd6TYj1vVg2biMYVt7my1HpoOtOweM309oNyNaxJ4Sblr"); 
 include_once "./bootstrap.php";
 
 include_once './index.html';
