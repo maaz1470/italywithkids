e523a2892121cd252a7613679000250055f1d655
Rahat Hossain <webmaazgmail.com>
2024-02-23 23:27:29 +0600

    update

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+define("NITROPACK_HOME_URL", "Your home page URL"); 
+define("NITROPACK_SITE_ID", "your site ID"); 
+define("NITROPACK_SITE_SECRET", "your site secret"); 
+include_once "./bootstrap.php";
+
+include_once './index.html';
